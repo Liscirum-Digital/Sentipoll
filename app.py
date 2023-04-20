7d3a3c33498e4cc9a06e46529af17b2352b00cdd
Jiscona-private <74877302+Jiscona-private@users.noreply.github.com>
2023-04-20 19:45:32 +0200

    Improved charts, added basic chart settings

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import datetime
 import csv
-from flask import Flask, render_template, flash, redirect, request, url_for, send_from_directory, session, make_response
+import requests
+from flask import Flask, render_template, jsonify, redirect, request
 from flask_sqlalchemy import SQLAlchemy
 
 import tubaerit_utils
@@ -21,9 +22,21 @@ class Surveys(db.Model):
     creator = db.Column(db.String(64), nullable=False)
     token = db.Column(db.String(64), unique=True, nullable=False)
     xName = db.Column(db.String(32))
+    xMin = db.Column(db.Integer, nullable=False) 
+    xMax = db.Column(db.Integer, nullable=False) 
     yName = db.Column(db.String(32), nullable=False)
+    yMin = db.Column(db.Integer, nullable=False) 
+    yMax = db.Column(db.Integer, nullable=False) 
     createdTime = db.Column(db.DateTime(), default=datetime.datetime.now()) 
     
+def readResults(token):
+    results = []
+    with open(f'results/{token}.csv', newline='') as csvfile:
+        resultsReader = csv.reader(csvfile, delimiter=' ')
+        for result in resultsReader:
+            results.append(result)
+    return results
+
 
 @app.route("/")
 def start():
@@ -40,42 +53,52 @@ def serve_survey(token):
 
     # returning page for data input
     accessedSurvey = Surveys.query.filter_by(token=token).first()
-    xName = accessedSurvey.xName
-    yName = accessedSurvey.yName
-    title = accessedSurvey.title
-    return render_template("access_survey.html", title=title, xName=xName, yName=yName)
+    return render_template("access_survey.html", title=accessedSurvey.title, xName=accessedSurvey.xName, xMin=accessedSurvey.xMin, xMax=accessedSurvey.xMax, yName=accessedSurvey.yName, yMin=accessedSurvey.yMin, yMax=accessedSurvey.yMax)
 
 @app.route("/create", methods=['GET', 'POST'])
 def create_survey():
     if request.method == 'POST':
         if (request.form["adminPassword"] == ADMIN_PASSWORD):
             # collecting data
-            title = request.form["title"]
             creator = "Admin" # to come
-            token = tubaerit_utils.generateToken(64)
-            xName = request.form["xName"]
-            yName = request.form["yName"]
+            token = tubaerit_utils.generateToken(8)
+            while (Surveys.query.filter_by(token=token).first()):
+                token = tubaerit_utils.generateToken(8) # making sure the token isnt already used
+            xMin = None
+            xMax = None
+            yMin = None
+            yMax = None
+            if (request.form['xMin']):
+                xMin = request.form['xMin']
+            if (request.form['xMax']):
+                xMax = request.form['xMax']
+            if (request.form['yMin']):
+                yMin = request.form['yMin']
+            if (request.form['xMax']):
+                yMax = request.form['yMax']
+
             # making database entry
-            newSurvey = Surveys(title=title, creator=creator, token=token, xName=xName, yName=yName)
+            newSurvey = Surveys(title=request.form["title"], creator=creator, token=token, xName=request.form["xName"], xMin=xMin, xMax=xMax, yName=request.form["yName"], yMin=yMin, yMax=yMax)
             db.session.add(newSurvey)
             db.session.commit()
             db.session.refresh(newSurvey)     
-            return render_template("succes.html") 
+            return render_template("survey_created.html", token=token) 
         return render_template("create_survey.html", error="Falsches Passwort.")
     return render_template("create_survey.html")
 
 @app.route("/results/<token>", methods=['GET', 'POST'])
-def show_results(token):
-    if request.method == 'POST':
-        # writing results into file
-        with open(f'results/{token}.csv', 'a', newline='') as csvfile:
-            resultsWriter = csv.writer(csvfile, delimiter=' ',)
-            resultsWriter.writerow([request.form['xInput'],  request.form['yInput']])
-        return render_template("success.html")
-
-    # returning page for data input
+def show_results(token):    
+    # getting information about the survey
     accessedSurvey = Surveys.query.filter_by(token=token).first()
-    xName = accessedSurvey.xName
-    yName = accessedSurvey.yName
-    title = accessedSurvey.title
-    return render_template("survey_results.html", title=title, xName=xName, yName=yName)
+    # getting results
+    gatheredData = readResults(token)
+    return render_template("results_survey.html", title=accessedSurvey.title, xName=accessedSurvey.xName, xMin=accessedSurvey.xMin, xMax=accessedSurvey.xMax, yName=accessedSurvey.yName, yMin=accessedSurvey.yMin, yMax=accessedSurvey.yMax, data=jsonify(gatheredData), token=token)
+
+@app.route('/update/<token>', methods=['GET'])
+def updateResults(token):
+    gatheredData = readResults(token)
+    print(gatheredData)
+    data = jsonify(gatheredData)
+    print(data)
+    # Return data as JSON response
+    return data
--- static/generate_charts.js
+++ static/generate_charts.js
@@ -1,22 +1,73 @@
-function bar(xName, yName, results) {
-  const ctx = document.getElementById('chart');
+var results = []
+var title = ""
+var xName = ""
+var yName = ""
 
-  new Chart(ctx, {
-      type: 'bar',
-      data: {
-        labels: [xName, yName],
-        datasets: [{
-          label: 'Anzahl',
-          data: results,
-          borderWidth: 1
-        }]
-      },
-      options: {
-        scales: {
-          y: {
-            beginAtZero: true
-          }
+
+function init(surveyTitle, x, y) {
+  google.charts.load('current', {'packages':['corechart']});
+  google.charts.setOnLoadCallback(drawChart);
+
+  title=surveyTitle;
+  xName=x;
+  yName=y;
+  setInterval(function() {
+    updatePage();
+  }, 1000);
+}
+
+// AJAX requests
+function updatePage() {
+    currentToken = document.getElementById('token').value;
+    fetch(`/update/${currentToken}`, {
+        method: 'GET',
+        headers: {
+        'Content-Type': 'application/json'
         }
+    })
+    .then(response => response.json())
+    .then(data => {
+        results = data;
+    })
+    .catch(error => console.error('Error:', error));
+
+    drawChart();
+}
+
+function drawChart() {
+  data = [[xName, yName]];
+
+  for (var i = 0; i < results.length; i++) {
+      result = [parseFloat(results[i][0]), parseFloat(results[i][1])]
+      data.push(result);
+  }
+
+  var chartData = google.visualization.arrayToDataTable(data);
+
+  var options = {
+    title: `${yName} in Abhängigkeit von ${xName}`,
+    hAxis: {
+      title: xName,
+      viewWindowMode:'explicit',
+      viewWindow: {
+        min: parseFloat(document.getElementById('xRangeMin').value),
+        max: parseFloat(document.getElementById('xRangeMax').value),
       }
-      });
+    },
+    vAxis: {
+      title: yName,
+      viewWindowMode:'explicit',
+      viewWindow: {
+        min: parseFloat(document.getElementById('yRangeMin').value),
+        max: parseFloat(document.getElementById('yRangeMax').value),
+      }
+    },
+    width: '100%',
+    height: '100%',
+    legend: 'none'
+  };
+
+  var chart = new google.visualization.ScatterChart(document.getElementById('chart_div'));
+
+  chart.draw(chartData, options);
 }
